a34e964818e977a866af8908cb5f5aecd7fd92b2
Rohan Jai <83573281+Rohanjai@users.noreply.github.com>
2022-08-04 07:19:36 +0530

    another solution in cpp

---
+++ Daily Test/XthElementInSpiralFormMatrix..cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+using namespace std;
+int main(int argc, char** argv)
+{
+    int r,c;
+    cin>>r>>c;
+    map<int,int> mp;
+    for(int i = 0 ;i<r;i++){
+        for(int j = 0 ;j<c;j++){
+            int val;
+            cin>>val;
+            mp[val]++;
+        }
+    }
+    for(auto &i: mp){
+        if(i.second>c){
+            cout<<"NO";
+            return 0;
+        }
+    }
+    cout<<"YES";
+}
