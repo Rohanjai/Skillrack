8949cfb3396be3b3eca08a0b59d25bf7e953783d
Rohan Jai <83573281+Rohanjai@users.noreply.github.com>
2022-09-16 17:19:02 +0530

    Flowchart logic 006

---
+++ Daily Challenges/flowchart-logic-006.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int N, ctr ; 
+    cin>>N;  
+    for(ctr=1;ctr<=n && ctr%2!=0 ;ctr+=2){
+        cout<<ctr;
+    }
+    return 0;
+}
