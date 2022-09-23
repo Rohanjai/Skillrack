3e05cb826f3971ef1dddb3af437d178cf38157bb
Rohan Jai <83573281+Rohanjai@users.noreply.github.com>
2022-09-23 06:49:53 +0530

    Escape speed

---
+++ Daily Challenges/Escape-speed.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    double g,r,m;
+    cin>>g>>m>>r;
+    double ans=g*m*2;
+    printf("%.3f",sqrt(ans/r));
+}
