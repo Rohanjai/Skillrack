d4fd57f0832c5c8233b10c4c5d2a1d2d4e895419
Rohan Jai <83573281+Rohanjai@users.noreply.github.com>
2022-09-24 09:36:10 +0530

    Jumbled program 001

---
+++ Daily Challenges/jumbled-program-001.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n,max,ctr,curr;
+    scanf("%d %d",&n,&max);
+    for(ctr=2;ctr<=n;ctr++){
+        scanf("%d",&curr);
+        if(curr>max){
+            max=curr;
+        }
+    }
+    printf("%d",max);
+
+}
