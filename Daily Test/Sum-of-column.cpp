8374ec9eae6015690fd2177ff9c84894163a2719
Rohan Jai <83573281+Rohanjai@users.noreply.github.com>
2022-11-16 22:10:43 +0530

    Sum of columns

--- Daily Test/Sum-of-column.cpp
+++ Daily Test/Sum-of-column.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(int argc, char** argv)
 {
     int r,c,sum=0;
-    bool print=true;
+    bool print=false;
     cin>>r>>c;
     int arr[r][c];
     for(int i=0;i<r;i++){
@@ -14,19 +14,14 @@ int main(int argc, char** argv)
         }
     }
     for(int i=0;i<c;i++){
-        for(int j=0;j<r;j++){
-            if(arr[0][i]&1){
-                sum+=arr[j][i];
-                print=false;
-                continue;
-            }
-            if(!print){
+        if(arr[0][i]&1){
+            for(int j=0;j<r;j++){
                 sum+=arr[j][i];
             }
-        }
-        print=true;
+            print=true;
+        }  
     }
-    if(sum==0){
+    if(!print){
         cout<<-1;
     }
     else{
