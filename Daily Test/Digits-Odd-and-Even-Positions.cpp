43795af9d5935060eb0046f0b06e9a9b7d6cd26e
Rohan Jai <83573281+Rohanjai@users.noreply.github.com>
2022-11-17 22:03:49 +0530

    Digits Odd and Even Positions

---
+++ Daily Test/Digits-Odd-and-Even-Positions.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string s;
+    cin>>s;
+    for(int i=0;i<s.length();i++){
+        if((i+1)&1){
+            cout<<s[i]<<" ";
+        }
+    }
+    cout<<endl;
+    for(int i=0;i<s.length();i++){
+        if(!((i+1)&1)){
+            cout<<s[i]<<" ";
+        }
+    }
+
+
+}
