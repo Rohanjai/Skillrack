f164260e77c5761aa4b4bf615935b3b9c54da187
Rohan Jai <83573281+Rohanjai@users.noreply.github.com>
2023-01-09 07:44:06 +0530

    Prepend and append Character

---
+++ Daily Challenges/Prepend-and-Append-Character.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string s;
+    char ch;
+    cin>>s>>ch;
+    if(s[0]!=ch){
+        cout<<ch;
+    }
+    cout<<s;
+    if(s[s.length()-1]!=ch){
+        cout<<ch;
+    }
+    
+
+
+}
