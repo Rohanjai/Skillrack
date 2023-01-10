2d4d472fd8844d0afd3cffe5b046e6a9d6ce5959
Rohan Jai <83573281+Rohanjai@users.noreply.github.com>
2023-01-10 09:56:14 +0530

    Arithmetic Progression till C

---
+++ Daily Challenges/Arithmetic-Progression-till-C.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    for(int i=a;i<=c;i+=b){
+        cout<<i<<" ";
+    }
+
+
+}
