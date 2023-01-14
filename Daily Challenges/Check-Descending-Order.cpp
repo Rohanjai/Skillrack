73e054b5d87b0995cb3680c1c6a04bdef5f8e819
Rohan Jai <83573281+Rohanjai@users.noreply.github.com>
2023-01-14 09:54:43 +0530

    Check Descending Order

---
+++ Daily Challenges/Check-Descending-Order.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=1;i<n;i++){
+        if(arr[i-1]<arr[i]){
+            cout<<"no";
+            return 0;
+        }
+    }
+    cout<<"yes";
+
+
+}
