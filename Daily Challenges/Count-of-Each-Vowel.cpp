d36d939d61088912beeb47b6738cad8a1a8b074c
Rohan Jai <83573281+Rohanjai@users.noreply.github.com>
2023-01-22 09:35:44 +0530

    Count of Each Vowel

---
+++ Daily Challenges/Count-of-Each-Vowel.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string s;
+    cin>>s;
+    cout<<"a "<<count(s.begin(),s.end(),'a')<<endl;
+    cout<<"e "<<count(s.begin(),s.end(),'e')<<endl;
+    cout<<"i "<<count(s.begin(),s.end(),'i')<<endl;
+    cout<<"o "<<count(s.begin(),s.end(),'o')<<endl;
+    cout<<"u "<<count(s.begin(),s.end(),'u')<<endl;
+
+
+}
