54fd9a4d7938b8d80c7380ee19fec0eb752f1187
Rohan Jai <83573281+Rohanjai@users.noreply.github.com>
2023-01-27 22:04:12 +0530

    Odd Int Odd pos

---
+++ Daily Test/Odd-Int-Odd-Pos.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int t;
+        cin>>t;
+        if(t&1 and (i+1)&1){
+            cout<<t<<" ";
+        }
+    }
+
+
+}
