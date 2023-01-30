5b4f57fea8911bc1e0f3248350c8e77c4b357352
Rohan Jai <83573281+Rohanjai@users.noreply.github.com>
2023-01-30 06:17:53 +0530

    Previous K Integers

---
+++ Daily Challenges/Previous-K-Integers.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int n,k;
+    cin>>n>>k;
+    for(int i=n-k;i<=n;i++){
+        cout<<i<<" ";
+    }
+
+}
