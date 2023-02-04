d6ede33468f34c4e330d924f0c5044ea56c9a31a
Rohan Jai <83573281+Rohanjai@users.noreply.github.com>
2023-02-04 06:17:12 +0530

    Character at Position

---
+++ Daily Challenges/Character-at-Position.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        char ch='a'-1;
+        int t;
+        cin>>t;
+        cout<<(char)(ch+t);
+    }
+
+
+}
