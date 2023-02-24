a15305d81446cea17eff1e378a81551889101be8
Rohan Jai <83573281+Rohanjai@users.noreply.github.com>
2023-02-24 05:57:37 +0530

    Stabilize Voltage

---
+++ Daily Challenges/Stabilize-Voltage.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int x,y;
+    cin>>x>>y;
+    if(abs(x-y)<=50){
+        cout<<"yes";
+    }
+    else{
+        cout<<"no";
+    }
+
+
+}
