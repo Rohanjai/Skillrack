9ebc60d564f685ea7a08a723798317f98bfd9502
Rohan Jai <83573281+Rohanjai@users.noreply.github.com>
2023-06-24 05:33:53 +0530

    First-X-Characters-for-N-Times.cpp

---
+++ Daily Challenges/First-X-Characters-for-N-Times.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string s;
+    int x,n;
+    cin>>s>>x>>n;
+    for(int i=0;i<n;i++){
+        cout<<s.substr(0,x);
+    }
+
+
+}
