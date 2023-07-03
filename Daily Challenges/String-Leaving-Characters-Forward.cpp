8c7f830b97dbb00cd93d7b2fa4c5aaa0b86fbf6f
Rohan Jai <83573281+Rohanjai@users.noreply.github.com>
2023-07-03 06:37:25 +0530

    String-Leaving-Characters-Forward.cpp

---
+++ Daily Challenges/String-Leaving-Characters-Forward.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string s;
+    cin>>s;
+    for(int i=0,ctr=2;i<s.size();i+=ctr,ctr++){
+        cout<<s[i];
+    }
+
+
+}
